93a623ac964d532cf8e3c1b493f12e4b342aa691
kitaee <dlrlxo999@naver.com>
2021-11-10 05:33:52 +0900

    third commit

--- src/main/java/kitae/foolaccount/controller/BoardController.java
+++ src/main/java/kitae/foolaccount/controller/BoardController.java
@@ -5,9 +5,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.awt.print.Pageable;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -15,16 +18,31 @@ import java.util.ArrayList;
 public class BoardController {
 
     @GetMapping("/boards")
-    public String boardView(){
+    public String boardView(Model model){
 
-        final String domesticStockUrl = "https://kr.investing.com/stock-screener/?sp=country::11|sector::a|industry::a|equityType::a%3Ceq_market_cap;1";
+
+
+        final String domesticStockUrl = "https://kr.investing.com/equities/south-korea";
         Connection domesticStock = Jsoup.connect(domesticStockUrl);
         ArrayList<String> domesticStockList = new ArrayList<>();
+        ArrayList<Integer> domesticStockPriceList = new ArrayList<>();
+        ArrayList<String> domesticStockInformationList = new ArrayList<>();
 
         try{
             Document domesticCurrency = domesticStock.get();
-            Elements domesticCurrencyInformationList = domesticCurrency.select("#resultsTable tbody tr");
-            System.out.println(domesticCurrencyInformationList);
+            Elements domesticCurrencyInformationList = domesticCurrency.select("#cross_rate_markets_stocks_1 td");
+            for(Element company : domesticCurrencyInformationList){
+                domesticStockInformationList.add(company.text());
+            }
+            for(int k=0;k<50;k++){
+                domesticStockList.add(domesticStockInformationList.get(10*k+1));
+                String string_price = domesticStockInformationList.get(10*k+2).replace(",", "");
+                string_price = string_price.replace(".00", "");
+                int int_value = Integer.parseInt(string_price);
+                domesticStockPriceList.add(int_value);
+            }
+//            System.out.println(domesticStockList);
+//            System.out.println(domesticStockPriceList);
 
         }catch(
                 IOException e){
@@ -32,7 +50,8 @@ public class BoardController {
         }
 
 
-
+        model.addAttribute("domesticStockList", domesticStockList);
+        model.addAttribute("domesticStockPriceList", domesticStockPriceList);
 
         return "board";
     }
